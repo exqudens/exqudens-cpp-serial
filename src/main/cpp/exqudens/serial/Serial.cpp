7a4000f0dbec3d0ce3d0c4e865ef4a827fb40793
exqudens <exqudens@gmail.com>
2024-06-29 00:05:18 -0700

    step 7

--- src/main/cpp/exqudens/serial/Serial.cpp
+++ src/main/cpp/exqudens/serial/Serial.cpp
@@ -11,227 +11,267 @@
 #include "exqudens/serial/versions.hpp"
 
 #define CALL_INFO std::string(__FUNCTION__) + "(" + std::filesystem::path(__FILE__).filename().string() + ":" + std::to_string(__LINE__) + ")"
+#define LOGGER_ID "exqudens.Serial"
+#define LOGGER_LEVEL_ERROR 2
+#define LOGGER_LEVEL_DEBUG 5
 
 namespace exqudens {
 
-  void Serial::setLogFunction(
-    const std::function<void(
-      const std::string& file,
-      const size_t& line,
-      const std::string& function,
-      const std::string& id,
-      const unsigned short& level,
-      const std::string& message
-    )>& value
-  ) {
-    logFunction = value;
-  }
+    Serial::Serial(const bool& autoClose): autoClose(autoClose) {}
 
-  bool Serial::isSetLogFunction() {
-    return (bool) logFunction;
-  }
+    Serial::Serial(): Serial(true) {}
 
-  std::string Serial::getVersion() {
-    return std::to_string(PROJECT_VERSION_MAJOR) + "." + std::to_string(PROJECT_VERSION_MINOR) + "." + std::to_string(PROJECT_VERSION_PATCH);
-  }
+    void Serial::setLogFunction(
+        const std::function<void(
+            const std::string& file,
+            const size_t& line,
+            const std::string& function,
+            const std::string& id,
+            const unsigned short& level,
+            const std::string& message
+        )>& value
+    ) {
+        logFunction = value;
+    }
 
-  std::vector<std::map<std::string, std::string>> Serial::listPorts() {
-    try {
-      std::vector<std::map<std::string, std::string>> results;
-      std::vector<serial::PortInfo> portInfos = serial::list_ports();
+    bool Serial::isSetLogFunction() {
+        return (bool) logFunction;
+    }
 
-      for (const serial::PortInfo& portInfo : portInfos) {
-        std::map<std::string, std::string> result;
-        result.insert({"port", portInfo.port});
-        result.insert({"description", portInfo.description});
-        result.insert({"hardware-id", portInfo.hardware_id});
+    std::string Serial::getVersion() {
+        return std::to_string(PROJECT_VERSION_MAJOR) + "." + std::to_string(PROJECT_VERSION_MINOR) + "." + std::to_string(PROJECT_VERSION_PATCH);
+    }
 
-        if (logFunction) {
-          std::string message = "{";
-          message += "'port': '" + result.at("port") + "', ";
-          message += "'description': '" + result.at("description") + "', ";
-          message += "'hardware-id': '" + result.at("hardware_id") + "'";
-          message += "}";
+    std::vector<std::map<std::string, std::string>> Serial::listPorts() {
+        try {
+            std::vector<std::map<std::string, std::string>> results;
+            std::vector<serial::PortInfo> portInfos = serial::list_ports();
+
+            for (const serial::PortInfo& portInfo : portInfos) {
+                std::map<std::string, std::string> result;
+                result.insert({"port", portInfo.port});
+                result.insert({"description", portInfo.description});
+                result.insert({"hardware-id", portInfo.hardware_id});
+
+                if (logFunction) {
+                    std::string message = "{";
+                    message += "'port': '" + result.at("port") + "', ";
+                    message += "'description': '" + result.at("description") + "', ";
+                    message += "'hardware-id': '" + result.at("hardware_id") + "'";
+                    message += "}";
+                }
+
+                results.emplace_back(result);
+            }
+
+            return results;
+        } catch (...) {
+            std::throw_with_nested(std::runtime_error(CALL_INFO));
         }
-
-        results.emplace_back(result);
-      }
-
-      return results;
-    } catch (...) {
-      std::throw_with_nested(std::runtime_error(CALL_INFO));
     }
-  }
 
-  void Serial::open(
-    const std::string& port,
-    const unsigned int& baudRate,
-    const unsigned int& timeoutInterByte,
-    const unsigned int& timeoutReadConstant,
-    const unsigned int& timeoutReadMultiplier,
-    const unsigned int& timeoutWriteConstant,
-    const unsigned int& timeoutWriteMultiplier,
-    const unsigned int& biteSize,
-    const unsigned int& parity,
-    const unsigned int& stopBits,
-    const unsigned int& flowControl
-  ) {
-    try {
-      serial::bytesize_t internalBiteSize = serial::eightbits;
+    void Serial::open(
+        const std::string& port,
+        const unsigned int& baudRate,
+        const unsigned int& timeoutInterByte,
+        const unsigned int& timeoutReadConstant,
+        const unsigned int& timeoutReadMultiplier,
+        const unsigned int& timeoutWriteConstant,
+        const unsigned int& timeoutWriteMultiplier,
+        const unsigned int& biteSize,
+        const unsigned int& parity,
+        const unsigned int& stopBits,
+        const unsigned int& flowControl
+    ) {
+        try {
+            log(__FILE__, __LINE__, __FUNCTION__, LOGGER_ID, LOGGER_LEVEL_DEBUG, "port: '" + port + "'");
 
-      if (biteSize == 8) {
-        internalBiteSize = serial::eightbits;
-      } else if (biteSize == 7) {
-        internalBiteSize = serial::sevenbits;
-      } else if (biteSize == 6) {
-        internalBiteSize = serial::sixbits;
-      } else if (biteSize == 5) {
-        internalBiteSize = serial::fivebits;
-      } else {
-        throw std::invalid_argument("biteSize");
-      }
+            serial::bytesize_t internalBiteSize = serial::eightbits;
 
-      serial::parity_t internalParity = serial::parity_none;
+            if (biteSize == 8) {
+                internalBiteSize = serial::eightbits;
+            } else if (biteSize == 7) {
+                internalBiteSize = serial::sevenbits;
+            } else if (biteSize == 6) {
+                internalBiteSize = serial::sixbits;
+            } else if (biteSize == 5) {
+                internalBiteSize = serial::fivebits;
+            } else {
+                throw std::invalid_argument("biteSize");
+            }
 
-      if (parity == 0) {
-        internalParity = serial::parity_none;
-      } else if (parity == 1) {
-        internalParity = serial::parity_odd;
-      } else if (parity == 2) {
-        internalParity = serial::parity_even;
-      } else {
-        throw std::invalid_argument("parity");
-      }
+            serial::parity_t internalParity = serial::parity_none;
 
-      serial::stopbits_t internalStopBits = serial::stopbits_one;
+            if (parity == 0) {
+                internalParity = serial::parity_none;
+            } else if (parity == 1) {
+                internalParity = serial::parity_odd;
+            } else if (parity == 2) {
+                internalParity = serial::parity_even;
+            } else {
+                throw std::invalid_argument("parity");
+            }
 
-      if (stopBits == 0) {
-        internalStopBits = serial::stopbits_one;
-      } else if (stopBits == 1) {
-        internalStopBits = serial::stopbits_one_point_five;
-      } else if (stopBits == 2) {
-        internalStopBits = serial::stopbits_two;
-      } else {
-        throw std::invalid_argument("stopBits");
-      }
+            serial::stopbits_t internalStopBits = serial::stopbits_one;
 
-      serial::flowcontrol_t internalFlowControl = serial::flowcontrol_none;
+            if (stopBits == 0) {
+                internalStopBits = serial::stopbits_one;
+            } else if (stopBits == 1) {
+                internalStopBits = serial::stopbits_one_point_five;
+            } else if (stopBits == 2) {
+                internalStopBits = serial::stopbits_two;
+            } else {
+                throw std::invalid_argument("stopBits");
+            }
 
-      if (flowControl == 0) {
-        internalFlowControl = serial::flowcontrol_none;
-      } else if (flowControl == 1) {
-        internalFlowControl = serial::flowcontrol_software;
-      } else if (flowControl == 2) {
-        internalFlowControl = serial::flowcontrol_hardware;
-      } else {
-        throw std::invalid_argument("flowControl");
-      }
+            serial::flowcontrol_t internalFlowControl = serial::flowcontrol_none;
 
-      object = std::make_unique<serial::Serial>(
-        port,
-        baudRate,
-        serial::Timeout(timeoutInterByte, timeoutReadConstant, timeoutReadMultiplier, timeoutWriteConstant, timeoutWriteMultiplier),
-        internalBiteSize,
-        internalParity,
-        internalStopBits,
-        internalFlowControl
-      );
-    } catch (...) {
-      std::throw_with_nested(std::runtime_error(CALL_INFO));
-    }
-  }
+            if (flowControl == 0) {
+                internalFlowControl = serial::flowcontrol_none;
+            } else if (flowControl == 1) {
+                internalFlowControl = serial::flowcontrol_software;
+            } else if (flowControl == 2) {
+                internalFlowControl = serial::flowcontrol_hardware;
+            } else {
+                throw std::invalid_argument("flowControl");
+            }
 
-  void Serial::open(const std::string& port, const unsigned int& timeoutSimple) {
-    try {
-      open(
-        port,
-        9600,
-        serial::Timeout::max(),
-        timeoutSimple,
-        0,
-        timeoutSimple,
-        0,
-        8,
-        0,
-        0,
-        0
-      );
-    } catch (...) {
-      std::throw_with_nested(std::runtime_error(CALL_INFO));
-    }
-  }
-
-  void Serial::open(const std::string& port) {
-    try {
-      open(
-        port,
-        9600,
-        0,
-        0,
-        0,
-        0,
-        0,
-        8,
-        0,
-        0,
-        0
-      );
-    } catch (...) {
-      std::throw_with_nested(std::runtime_error(CALL_INFO));
-    }
-  }
-
-  bool Serial::isOpen() {
-    try {
-      if (object) {
-        return object->isOpen();
-      }
-      return false;
-    } catch (...) {
-      std::throw_with_nested(std::runtime_error(CALL_INFO));
-    }
-  }
-
-  void Serial::close() {
-    try {
-      if (object) {
-        if (object->isOpen()) {
-          object->close();
+            object = std::make_unique<serial::Serial>(
+                port,
+                baudRate,
+                serial::Timeout(timeoutInterByte, timeoutReadConstant, timeoutReadMultiplier, timeoutWriteConstant, timeoutWriteMultiplier),
+                internalBiteSize,
+                internalParity,
+                internalStopBits,
+                internalFlowControl
+            );
+        } catch (...) {
+            std::throw_with_nested(std::runtime_error(CALL_INFO));
         }
-      }
-    } catch (...) {
-      std::throw_with_nested(std::runtime_error(CALL_INFO));
     }
-  }
 
-  size_t Serial::writeBytes(const std::vector<unsigned char>& bytes) {
-    try {
-      if (!isOpen()) {
-        throw std::runtime_error("device is not open");
-      }
-      size_t result = 0;
-      result = object->write(bytes);
-      return result;
-    } catch (...) {
-      std::throw_with_nested(std::runtime_error(CALL_INFO));
+    void Serial::open(const std::string& port, const unsigned int& timeoutSimple) {
+        try {
+            open(
+                port,
+                9600,
+                serial::Timeout::max(),
+                timeoutSimple,
+                0,
+                timeoutSimple,
+                0,
+                8,
+                0,
+                0,
+                0
+            );
+        } catch (...) {
+            std::throw_with_nested(std::runtime_error(CALL_INFO));
+        }
     }
-  }
 
-  std::vector<unsigned char> Serial::readBytes(const size_t& size) {
-    try {
-      if (!isOpen()) {
-        throw std::runtime_error("device is not open");
-      }
-      std::vector<unsigned char> result;
-      object->read(result, size);
-      return result;
-    } catch (...) {
-      std::throw_with_nested(std::runtime_error(CALL_INFO));
+    void Serial::open(const std::string& port) {
+        try {
+            open(
+                port,
+                9600,
+                0,
+                0,
+                0,
+                0,
+                0,
+                8,
+                0,
+                0,
+                0
+            );
+        } catch (...) {
+            std::throw_with_nested(std::runtime_error(CALL_INFO));
+        }
     }
-  }
 
-  Serial::~Serial() noexcept = default;
+    bool Serial::isOpen() {
+        try {
+            if (object) {
+                return object->isOpen();
+            }
+            return false;
+        } catch (...) {
+            std::throw_with_nested(std::runtime_error(CALL_INFO));
+        }
+    }
+
+    void Serial::close() {
+        try {
+            if (object) {
+                if (object->isOpen()) {
+                    object->close();
+                }
+            }
+        } catch (...) {
+            std::throw_with_nested(std::runtime_error(CALL_INFO));
+        }
+    }
+
+    size_t Serial::writeBytes(const std::vector<unsigned char>& bytes) {
+        try {
+            if (!isOpen()) {
+                throw std::runtime_error("device is not open");
+            }
+            size_t result = 0;
+            result = object->write(bytes);
+            return result;
+        } catch (...) {
+            std::throw_with_nested(std::runtime_error(CALL_INFO));
+        }
+    }
+
+    std::vector<unsigned char> Serial::readBytes(const size_t& size) {
+        try {
+            if (!isOpen()) {
+                throw std::runtime_error("device is not open");
+            }
+            std::vector<unsigned char> result;
+            object->read(result, size);
+            return result;
+        } catch (...) {
+            std::throw_with_nested(std::runtime_error(CALL_INFO));
+        }
+    }
+
+    Serial::~Serial() noexcept {
+        if (autoClose) {
+            try {
+                close();
+            } catch (const std::exception& e) {
+                log(__FILE__, __LINE__, __FUNCTION__, LOGGER_ID, LOGGER_LEVEL_ERROR, "Error in destructor on call function: 'close': '" + std::string(e.what()) + "'");
+            } catch (...) {
+                log(__FILE__, __LINE__, __FUNCTION__, LOGGER_ID, LOGGER_LEVEL_ERROR, "Unknown error in destructor on call function: 'close'");
+            }
+        }
+    }
+
+    void Serial::log(
+        const std::string& file,
+        const size_t& line,
+        const std::string& function,
+        const std::string& id,
+        const unsigned short& level,
+        const std::string& message
+    ) {
+        try {
+            if (logFunction) {
+                std::string internalFile = std::filesystem::path(file).filename().string();
+                logFunction(internalFile, line, function, id, level, message);
+            }
+        } catch (...) {
+            std::throw_with_nested(std::runtime_error(CALL_INFO));
+        }
+    }
 
 }
 
 #undef CALL_INFO
+#undef LOGGER_ID
+#undef LOGGER_LEVEL_ERROR
+#undef LOGGER_LEVEL_DEBUG
--- src/main/cpp/exqudens/serial/Serial.hpp
+++ src/main/cpp/exqudens/serial/Serial.hpp
@@ -12,78 +12,82 @@
 
 namespace exqudens {
 
-  class EXQUDENS_SERIAL_EXPORT Serial : public virtual ISerial {
+   class EXQUDENS_SERIAL_EXPORT Serial : public virtual ISerial {
 
-    private:
+      private:
 
-      std::function<void(
-        const std::string& file,
-        const size_t& line,
-        const std::string& function,
-        const std::string& id,
-        const unsigned short& level,
-        const std::string& message
-      )> logFunction;
-      /*std::string port;
-      unsigned int baudRate = 9600;
-      unsigned int timeoutInterByte = 0;
-      unsigned int timeoutReadConstant = 0;
-      unsigned int timeoutReadMultiplier = 0;
-      unsigned int timeoutWriteConstant = 0;
-      unsigned int timeoutWriteMultiplier = 0;
-      unsigned int biteSize = 8;
-      unsigned int parity = 0;
-      unsigned int stopBits = 0;
-      unsigned int flowControl = 0;*/
-      std::unique_ptr<serial::Serial> object = nullptr;
+         std::function<void(
+            const std::string& file,
+            const size_t& line,
+            const std::string& function,
+            const std::string& id,
+            const unsigned short& level,
+            const std::string& message
+         )> logFunction;
+         bool autoClose = false;
+         std::unique_ptr<serial::Serial> object = nullptr;
 
-    public:
+      public:
 
-      void setLogFunction(
-          const std::function<void(
-              const std::string&,
-              const size_t&,
-              const std::string&,
-              const std::string&,
-              const unsigned short&,
-              const std::string&
-          )> &value
-      ) override;
+         Serial(const bool& autoClose);
+         Serial();
 
-      bool isSetLogFunction() override;
+         void setLogFunction(
+               const std::function<void(
+                     const std::string&,
+                     const size_t&,
+                     const std::string&,
+                     const std::string&,
+                     const unsigned short&,
+                     const std::string&
+               )> &value
+         ) override;
 
-      std::string getVersion() override;
+         bool isSetLogFunction() override;
 
-      std::vector<std::map<std::string, std::string>> listPorts() override;
+         std::string getVersion() override;
 
-      void open(
-        const std::string& port,
-        const unsigned int& baudRate,
-        const unsigned int& timeoutInterByte,
-        const unsigned int& timeoutReadConstant,
-        const unsigned int& timeoutReadMultiplier,
-        const unsigned int& timeoutWriteConstant,
-        const unsigned int& timeoutWriteMultiplier,
-        const unsigned int& biteSize,
-        const unsigned int& parity,
-        const unsigned int& stopBits,
-        const unsigned int& flowControl
-      ) override;
+         std::vector<std::map<std::string, std::string>> listPorts() override;
 
-      void open(const std::string& port, const unsigned int& timeoutSimple) override;
+         void open(
+            const std::string& port,
+            const unsigned int& baudRate,
+            const unsigned int& timeoutInterByte,
+            const unsigned int& timeoutReadConstant,
+            const unsigned int& timeoutReadMultiplier,
+            const unsigned int& timeoutWriteConstant,
+            const unsigned int& timeoutWriteMultiplier,
+            const unsigned int& biteSize,
+            const unsigned int& parity,
+            const unsigned int& stopBits,
+            const unsigned int& flowControl
+         ) override;
 
-      void open(const std::string& port) override;
+         void open(const std::string& port, const unsigned int& timeoutSimple) override;
 
-      bool isOpen() override;
+         void open(const std::string& port) override;
 
-      void close() override;
+         bool isOpen() override;
 
-      size_t writeBytes(const std::vector<unsigned char>& bytes) override;
+         void close() override;
 
-      std::vector<unsigned char> readBytes(const size_t& size) override;
+         size_t writeBytes(const std::vector<unsigned char>& bytes) override;
 
-      ~Serial() noexcept override;
+         std::vector<unsigned char> readBytes(const size_t& size) override;
 
-  };
+         ~Serial() noexcept override;
+
+      private:
+
+         void log(
+            const std::string& file,
+            const size_t& line,
+            const std::string& function,
+            const std::string& id,
+            const unsigned short& level,
+            const std::string& message
+         );
+
+   };
 
 }
